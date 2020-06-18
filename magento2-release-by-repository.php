6ffb7271bb5754b905acde28567b610168b6ef4f
Jisse Reitsma <jisse@yireo.com>
2020-06-18 16:55:44 +0200

    Additional validation

--- magento2-release-by-repository.php
+++ magento2-release-by-repository.php
@@ -9,8 +9,14 @@ require_once __DIR__ . '/vendor/autoload.php';
 FilesystemResolver::getInstance()->setRootFolder(dirname(__DIR__));
 
 $repositoryName = (string)$argv[1];
-if (!in_array($repositoryName,RepositoryList::getRepositories(true))) {
-    throw new RuntimeException('Invalid repository name');
+$allowedRepositories = RepositoryList::getRepositories(true);
+if (!in_array($repositoryName, RepositoryList::getRepositories(true))) {
+    throw new RuntimeException(
+        'Invalid repository name "' . $repositoryName . '". Repository should be one of the following: ' . implode(
+            ' ',
+            $allowedRepositories
+        )
+    );
 }
 
 $repository = RepositoryList::getRepositoryByName($repositoryName);
